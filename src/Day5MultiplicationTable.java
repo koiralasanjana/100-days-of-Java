68a57f1267c9f9fa96abd8d60a1be14e12c6b4be
Sanjana <67232409+koiralasanjana@users.noreply.github.com>
2024-08-15 13:24:13 +0545

    Day5-Multiplication table

--- src/Day5MultiplicationTable.java
+++ src/Day5MultiplicationTable.java
@@ -1,2 +1,20 @@
+import java.util.Scanner;
+
+//Write a program that prints the multiplication tables of a given number
+// up to 10.
 public class Day5MultiplicationTable {
+    public void multiplication(int num1){
+        for(int i=1; i<=10;i++) {
+            int multiply = num1 * i;
+            System.out.println(num1 + "*" + i + "=" + multiply);
+        }
+    }
+    public static void main(String[] args){
+        System.out.println("Enter desired number to generate multiplication table: ");
+        Scanner sc = new Scanner(System.in);
+        int num=sc.nextInt();
+        Day5MultiplicationTable multiply = new Day5MultiplicationTable();
+        multiply.multiplication(num);
+    }
 }
+
