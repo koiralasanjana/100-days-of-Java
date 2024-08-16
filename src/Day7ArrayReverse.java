18537ec92d8b9c8ab1239de0896e4f427904b46f
Sanjana <67232409+koiralasanjana@users.noreply.github.com>
2024-08-16 10:49:30 +0545

    Complete-day7-day8

--- src/Day7ArrayReverse.java
+++ src/Day7ArrayReverse.java
@@ -2,7 +2,25 @@
 
 public class Day7ArrayReverse {
 
-    public static void main(String[] args){
+    public void beforeSwap(int[] myArray){
+        System.out.println("Before swap: ");
+        for (int i=0; i<5;i++){
+            System.out.print(myArray[i]+"  ");
+        }
+        System.out.println("\n----------------------------");
+    }
 
+    public void afterSwap(int[] myArray){
+        System.out.println("After swap: ");
+        for (int i=4; i>=0;i--){
+            System.out.print(myArray[i]+"  ");
+        }
+    }
+
+    public static void main(String[] args){
+        int []myArray = new int[]{10,20,30,40,50};
+        Day7ArrayReverse before = new Day7ArrayReverse();
+        before.beforeSwap(myArray);
+        before.afterSwap(myArray);
     }
 }
--- src/Day8SecondLargestNumber.java
+++ src/Day8SecondLargestNumber.java
@@ -1,2 +1,22 @@
+//Write a program to find the second-largest number in the array of integers.
+
 public class Day8SecondLargestNumber {
+    public void testSecondLargest(int[] number){
+        for (int i = 0; i < number.length; i++) {
+            for (int j = 0; j < number.length; j++) {
+                if(number[i]>number[j]){
+                    int temp;
+                    temp = number[i];
+                    number[j]=temp;
+                    number[i]=number[j];
+                }
+            }
+        }
+        System.out.println(number[number.length-2]);
+    }
+    public static void main(String[] args){
+        int[] numbers=new int[]{30,20,60,500,100};
+        Day8SecondLargestNumber secondLargestNumber= new Day8SecondLargestNumber();
+        secondLargestNumber.testSecondLargest(numbers);
+    }
 }
