8e7abe10762b5ae1b23d54f5f1ef25dd139e1c46
Sanjana <67232409+koiralasanjana@users.noreply.github.com>
2024-09-20 11:11:36 +0545

    Day12-HashMap completed

--- src/Day12HashMap.java
+++ src/Day12HashMap.java
@@ -1,17 +1,30 @@
 //Write a program that uses a 'HashMap' to count the frequency of each character in a string.
 
 import java.util.Scanner;
+import java.util.HashMap;
+import java.util.Map;
+
 
 public class Day12HashMap {
-    public void hashMap(String myString){
-        System.out.println("My String: "+myString);
+    public static Map<Character, Integer> hashMap(String myString) {
+        Map<Character, Integer> frequencyMap = new HashMap<>();
+        myString = myString.toLowerCase();
+        for (char c : myString.toCharArray()) {
+            // If the character is already in the map, increment its count
+            frequencyMap.put(c, frequencyMap.getOrDefault(c, 0) + 1);
+        }
+        return frequencyMap;
     }
 
     public static void main(String[] args){
         System.out.println("Please input your string: ");
         Scanner sc=new Scanner(System.in);
         String myString=sc.nextLine();
-        Day12HashMap hashMap = new Day12HashMap();
-        hashMap.hashMap(myString);
+        Map<Character, Integer> frequencyMap = hashMap(myString);
+
+        // Output the character frequencies
+        for (Map.Entry<Character, Integer> entry : frequencyMap.entrySet()) {
+            System.out.println("Character: '" + entry.getKey() + "' | Frequency: " + entry.getValue());
+        }
     }
 }
